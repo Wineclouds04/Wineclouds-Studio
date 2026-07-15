5f578ddbe9411aa69585b4572acb3b6e2a2c2103
wineclouds <jqw5481@gmail.com>
2026-07-15 15:51:36 +0800

    chore: archive v1.0.3

--- src/WinecloudsStudio/MainWindow.xaml.cs
+++ src/WinecloudsStudio/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using WinecloudsStudio.Pages;
+using WinecloudsStudio.Services.Implementation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -14,15 +15,30 @@ public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
-        InitializeComponent();
+        Logger.Info("MainWindow", "MainWindow initialization started");
 
-        ExtendsContentIntoTitleBar = true;
-        SetTitleBar(AppTitleBar);
+        try
+        {
+            Logger.Info("MainWindow", "Loading XAML components");
+            InitializeComponent();
 
-        AppWindow.SetIcon("Assets/AppIcon.ico");
+            Logger.Info("MainWindow", "Configuring title bar");
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
 
-        NavigateTo("home");
-        NavigationView.SelectedItem = NavigationView.MenuItems[1];
+            Logger.Info("MainWindow", "Loading application icon");
+            AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets", "AppIcon.ico"));
+
+            Logger.Info("MainWindow", "Navigating to home page");
+            NavigateTo("home");
+            NavigationView.SelectedItem = NavigationView.MenuItems[1];
+            Logger.Info("MainWindow", "MainWindow initialization completed");
+        }
+        catch (Exception exception)
+        {
+            Logger.Error("MainWindow", $"MainWindow initialization failed: {exception}");
+            throw;
+        }
     }
 
     private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
